13bc39da709274c929efc1ea9819d62d1a8bdd5b
iPersona <freedom2028@gmail.com>
2017-06-19 22:38:01 +0800

    Add: tmux copy-past support

--- init.sh
+++ init.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-ln -s ~/.tmux/tmux.conf ~/.tmux.conf
+
+# symbol link to ~/.tmux.conf
+tmuxConfLn=~/.tmux.conf
+if [ -L $tmuxConfLn ]; then
+  rm $tmuxConfLn
+fi
+ln -s ~/.tmux/tmux.conf $tmuxConfLn
+
+# install plugins
+tmuxPlugin=~/.tmux/plugins
+if [ -d $tmuxPlugin ]; then
+  rm -rf $tmuxPlugin
+fi
+git clone https://github.com/tmux-plugins/tpm $tmuxPlugin/tpm
+git clone https://github.com/tmux-plugins/tmux-sensible.git $tmuxPlugin/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-yank $tmuxPlugin/tmux-yank
+
+
+# reload config
+tmux source $tmuxConfLn
